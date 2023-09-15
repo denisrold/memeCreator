9893d14d9bac4f67ef83850d725a9b08c61e1675
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-15 20:07:44 -0300

    export imagen

--- meme-creator/memecreator/src/App.js
+++ meme-creator/memecreator/src/App.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import html2canvas from "html2canvas";
 
 function App() {
   const [linea1, setLinea1] = useState("");
   const [linea2, setLinea2] = useState("");
   const [imagen, setImagen] = useState("");
-  const onChangeIAmgen = function (event) {
-    setImagen(event.target.value);
+
+  const onClickExport = (e) => {
+    html2canvas(document.querySelector("#meme")).then((canvas) => {
+      document.body.appendChild(canvas);
+    });
   };
   return (
     <div className="App">
-      <select onChange={onChangeIAmgen}>
+      <select onChange={(e) => setImagen(e.target.value)}>
         <option>Seleccionar meme</option>
         <option value="futurama">Futurama</option>
         <option value="fire">Casa en llamas</option>
@@ -38,8 +42,8 @@ function App() {
         }}
       ></input>
       <br />
-      <button>Exportar</button>
-      <div className="meme">
+      <button onClick={onClickExport}>Exportar</button>
+      <div className="meme" id="meme">
         <span>{linea1}</span>
         <br />
         <span>{linea2}</span>
