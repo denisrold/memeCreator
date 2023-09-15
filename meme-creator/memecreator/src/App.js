95a448a9e77032560885cc64d877e506b24dc490
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-15 19:11:37 -0300

    Estructura memes

--- meme-creator/memecreator/src/App.js
+++ meme-creator/memecreator/src/App.js
@@ -2,10 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return;
-  <div className="App">
-    // select pickr //input text first //input text last /export image
-  </div>;
+  return (
+    <div className="App">
+      // select pickr
+      <select>
+        <option value="futurama">Futurama</option>
+        <option value="fire">Casa en llamas</option>
+        <option value="history">History channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="smart">Smart Guy</option>
+        <option value="philosoraptor">Philosoraptor</option>
+      </select>
+      <input type="text"></input>
+      <input type="text"></input>
+      <button>Exportar</button>
+    </div>
+  );
 }
 
 export default App;
